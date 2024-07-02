b50fb44acd276acb0ba8af0f5618329edc6f2d4b
f0kes <f0kesppokes@gmail.com>
2024-07-02 20:24:01 +0300

    +

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,12 @@
 sudo docker compose down
-git pull && cd ComfyUI-Launcher/ && git pull && cd .. && cd fil_anim_starter/ && git pull && cd ..
+start_dir=$(pwd)
+
+find . -type d | while read dir; do
+    if [ -d "$dir/.git" ]; then
+        echo "Updating $dir"
+        (cd "$dir" && git pull)
+        cd "$start_dir"
+    fi
+done
+
 sudo docker compose up --build -d
