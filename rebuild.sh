71abf5d2a8874baa2917b98a03d418a820c5e413
f0kes <f0kesppokes@gmail.com>
2024-07-02 20:21:45 +0300

    +

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
 sudo docker compose down
-git pull && cd ComfyUI-Launcher/ && git pull && cd ..
+git pull && cd ComfyUI-Launcher/ && git pull && cd .. && cd fil_anim_starter/ && git pull && cd ..
 sudo docker compose up --build -d
